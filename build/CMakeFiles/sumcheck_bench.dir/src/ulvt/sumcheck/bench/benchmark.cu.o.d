0324c7533faf63dfed5bea536b388212b9d5ebc9
Andrew Fan <im-afan@users.noreply.github.com>
2025-07-09 14:18:14 -0400

    checkpoint

--- build/CMakeFiles/sumcheck_bench.dir/src/ulvt/sumcheck/bench/benchmark.cu.o.d
+++ build/CMakeFiles/sumcheck_bench.dir/src/ulvt/sumcheck/bench/benchmark.cu.o.d
@@ -306,4 +306,32 @@ CMakeFiles/sumcheck_bench.dir/src/ulvt/sumcheck/bench/benchmark.cu.o : /home/int
     /usr/include/c++/12/cstring \
     /home/intern-04/binius-gpu/src/ulvt/sumcheck/bench/../core/core.cuh \
     /home/intern-04/binius-gpu/src/ulvt/sumcheck/bench/../core/../utils/constants.hpp \
-    /home/intern-04/binius-gpu/src/ulvt/sumcheck/bench/../core/kernels.cuh
+    /home/intern-04/binius-gpu/src/ulvt/sumcheck/bench/../core/kernels.cuh \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvToolsExt.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxLinkOnce.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxTypes.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxImpl.h \
+    /usr/include/unistd.h \
+    /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+    /usr/include/x86_64-linux-gnu/bits/environments.h \
+    /usr/include/x86_64-linux-gnu/bits/confname.h \
+    /usr/include/x86_64-linux-gnu/bits/getopt_posix.h \
+    /usr/include/x86_64-linux-gnu/bits/getopt_core.h \
+    /usr/include/x86_64-linux-gnu/bits/unistd.h \
+    /usr/include/x86_64-linux-gnu/bits/unistd_ext.h \
+    /usr/include/linux/close_range.h \
+    /usr/include/dlfcn.h \
+    /usr/include/x86_64-linux-gnu/bits/dlfcn.h \
+    /usr/include/x86_64-linux-gnu/bits/dl_find_object.h \
+    /usr/include/fcntl.h \
+    /usr/include/x86_64-linux-gnu/bits/fcntl.h \
+    /usr/include/x86_64-linux-gnu/bits/fcntl-linux.h \
+    /usr/include/x86_64-linux-gnu/bits/types/struct_iovec.h \
+    /usr/include/linux/falloc.h \
+    /usr/include/x86_64-linux-gnu/bits/stat.h \
+    /usr/include/x86_64-linux-gnu/bits/struct_stat.h \
+    /usr/include/x86_64-linux-gnu/bits/fcntl2.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxInitDecls.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxImplCore.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxInitDefs.h \
+    /usr/local/cuda-12.6/bin/../targets/x86_64-linux/include/nvtx3/nvtxDetail/nvtxInit.h
--- src/ulvt/sumcheck/bench/benchmark.cu
+++ src/ulvt/sumcheck/bench/benchmark.cu
@@ -69,17 +69,17 @@ void benchmark(int num_runs) {
 }
 
 int main() {
-	benchmark<20, 2>(10);
-	benchmark<20, 3>(10);
+	//benchmark<20, 2>(10);
+	//benchmark<20, 3>(10);
 	benchmark<20, 4>(10);
 
-	benchmark<24, 2>(5);
+	/*benchmark<24, 2>(5);
 	benchmark<24, 3>(5);
 	benchmark<24, 4>(5);
 
 	benchmark<28, 2>(2);
 	benchmark<28, 3>(2);
-	benchmark<28, 4>(2);
+	benchmark<28, 4>(2);*/
 
 	return 0;
 }

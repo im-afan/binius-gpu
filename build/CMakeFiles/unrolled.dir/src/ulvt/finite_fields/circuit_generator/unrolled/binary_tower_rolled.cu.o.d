98dab6ba8223331efafe9b85d69bfb7a5eac666f
Andrew Fan <im-afan@users.noreply.github.com>
2025-07-10 15:47:38 -0400

    working parallel multiplication. a bit slow though

--- build/CMakeFiles/unrolled.dir/src/ulvt/finite_fields/circuit_generator/unrolled/binary_tower_rolled.cu.o.d
+++ build/CMakeFiles/unrolled.dir/src/ulvt/finite_fields/circuit_generator/unrolled/binary_tower_rolled.cu.o.d
@@ -198,4 +198,5 @@ CMakeFiles/unrolled.dir/src/ulvt/finite_fields/circuit_generator/unrolled/binary
     /usr/include/stdint.h \
     /usr/include/x86_64-linux-gnu/bits/wchar.h \
     /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
-    /home/intern-04/binius-gpu/src/ulvt/finite_fields/circuit_generator/unrolled/binary_tower_rolled.cuh
+    /home/intern-04/binius-gpu/src/ulvt/finite_fields/circuit_generator/unrolled/binary_tower_rolled.cuh \
+    /home/intern-04/binius-gpu/src/ulvt/finite_fields/circuit_generator/unrolled/../constants.hpp
